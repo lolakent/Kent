7850c26621bc8013b85064f7426fa243490dc290
lolakent <ma755@kent.ac.uk>
2016-12-03 14:00:06 +0000

    Add files via upload

---
+++ JHTestTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JHTest;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lola
+ */
+public class JHTestTest {
+    
+    public JHTestTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class JHTest.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("Hello Lola Test");
+     
+    }   
+}
